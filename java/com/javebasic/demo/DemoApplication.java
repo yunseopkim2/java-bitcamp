e30f5c3f5ba0779317f2d341d65b84b8ef1dd9db
yunseopkim2 <dbstjqdlwksj@gmail.com>
2022-02-17 15:45:52 +0900

    2022-02-17 15:45

---
+++ java/com/javebasic/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.javebasic.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	publicstatic void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+		m
+}
