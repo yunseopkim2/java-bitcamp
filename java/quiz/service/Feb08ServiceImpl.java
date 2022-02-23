44c029df0d5637253df84a8770587355f9298f83
yunseopkim2 <dbstjqdlwksj@gmail.com>
2022-02-23 17:58:13 +0900

    2022-02-23 17:58

--- java/quiz/service/Feb08ServiceImpl.java
+++ java/quiz/service/Feb08ServiceImpl.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 public class Feb08ServiceImpl implements Feb08Service{
     @Override
     public void lotto(Scanner scanner) {
-
+        int lotto[] = new int[6];
+        for(int i = 0; i<=lotto.length; i++){
+            int num = (int)(Math.random()*46)+1;
+            lotto[i] = num;
+            for(int j = 0; j<i; j++){
+                if(lotto[i]==lotto[j]){
+                    i--;
+                }
+            }
+        }
+            for(int i = 0; i<lotto.length; i++){
+                System.out.println(lotto[i]);
+            }
     }
 
     @Override
@@ -15,6 +27,13 @@ public class Feb08ServiceImpl implements Feb08Service{
 
     @Override
     public void booking(Scanner scanner) {
+        String [][] seat = new String[7][7];
+        String [] list = {"A","B","C","D","E","F","G"};
+        for(int i = 0; i< seat.length; i++){
+            for(int j = 0; j< seat.length; j++){
+                seat[i][j] = " ㅁ ";
+            }
+        }
 
     }
 
